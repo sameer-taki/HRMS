5979ecfe144e0c9da8b0fdf717324de9b94f89b3
sameer-taki <sameerm.taki@gmail.com>
2025-06-24 10:52:08 +1200

    Create missing Next.js pages directory

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Fiji HRMS</h1>
+      <p>Welcome to the Human Resource Management System</p>
+    </div>
+  );
+}
